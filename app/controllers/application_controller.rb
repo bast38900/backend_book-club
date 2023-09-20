5e6f85f638d6d9743e5e7737b728674e44757bb9
Bast38900 <bast38900@edu.ucl.dk>
2023-09-20 11:56:13 +0200

    devise_controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+# Application controller class, parent to child controllers
 class ApplicationController < ActionController::API
+  # Make controllers respond to json, and not render view
+  respond_to :json
 end
